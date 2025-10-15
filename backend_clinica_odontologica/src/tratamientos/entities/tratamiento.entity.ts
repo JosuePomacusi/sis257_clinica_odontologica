4584e5af6d9036ee8eaa1a78ce1bf89ac20720eb
Josue Pomacusi <pomacusi7@gmail.com>
2025-10-15 18:21:06 -0400

    feat: tratamiento entity valida con TypeORM

--- backend_clinica_odontologica/src/tratamientos/entities/tratamiento.entity.ts
+++ backend_clinica_odontologica/src/tratamientos/entities/tratamiento.entity.ts
@@ -1,1 +1,22 @@
-export class Tratamiento {}
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('tratamientos')
+export class Tratamiento {
+    @PrimaryGeneratedColumn('identity')
+    id: number;
+
+    @Column('varchar', { length: 100 })
+    descripcion: string;
+
+    @Column('decimal', { precision: 9, scale: 2 })
+    costo: number;
+
+    @CreateDateColumn({ name: 'fecha_creacion' })
+    fechaCreacion: Date;
+
+    @UpdateDateColumn({ name: 'fecha_modificacion' })
+    fechaModificacion: Date;
+
+    @DeleteDateColumn({ name: 'fecha_eliminacion' })
+    fechaEliminacion: Date;
+}
