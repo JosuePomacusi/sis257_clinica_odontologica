32bb1185b35388820fd8ea0f465ee11dd6a784ca
Josue Pomacusi <pomacusi7@gmail.com>
2025-10-15 19:11:31 -0400

    feat: servicio implementado con operaciones CRUD funcionales

--- backend_clinica_odontologica/src/tratamientos/tratamientos.module.ts
+++ backend_clinica_odontologica/src/tratamientos/tratamientos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TratamientosService } from './tratamientos.service';
 import { TratamientosController } from './tratamientos.controller';
+import { Tratamiento } from './entities/tratamiento.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Tratamiento])],
   controllers: [TratamientosController],
   providers: [TratamientosService],
 })
--- backend_clinica_odontologica/src/tratamientos/tratamientos.service.ts
+++ backend_clinica_odontologica/src/tratamientos/tratamientos.service.ts
@@ -1,26 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTratamientoDto } from './dto/create-tratamiento.dto';
 import { UpdateTratamientoDto } from './dto/update-tratamiento.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tratamiento } from './entities/tratamiento.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TratamientosService {
-  create(createTratamientoDto: CreateTratamientoDto) {
-    return 'This action adds a new tratamiento';
+  constructor(@InjectRepository(Tratamiento) private tratamientosRepository: Repository<Tratamiento>) {}
+
+  async create(createTratamientoDto: CreateTratamientoDto): Promise<Tratamiento> {
+    const existente = await this.tratamientosRepository.findOneBy({ descripcion: createTratamientoDto.descripcion.trim() });
+    if (existente) throw new ConflictException('Ya existe un tratamiento con esa descripción');
+
+    const tratamiento = this.tratamientosRepository.create(createTratamientoDto);
+    return this.tratamientosRepository.save(tratamiento);
   }
 
-  findAll() {
-    return `This action returns all tratamientos`;
+  async findAll():Promise<Tratamiento[]> {
+    return this.tratamientosRepository.find({ order: { descripcion: 'ASC' } });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} tratamiento`;
+  async findOne(id: number):Promise <Tratamiento> {
+    const tratamiento = await this.tratamientosRepository.findOneBy({ id });
+    if (!tratamiento) throw new NotFoundException('Tratamiento no encontrado');
+    return tratamiento;
   }
 
-  update(id: number, updateTratamientoDto: UpdateTratamientoDto) {
-    return `This action updates a #${id} tratamiento`;
+  async update(id: number, updateTratamientoDto: UpdateTratamientoDto):Promise<Tratamiento> {
+    const tratamiento = await this.findOne(id);
+    Object.assign(tratamiento, updateTratamientoDto);
+    return this.tratamientosRepository.save(tratamiento);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} tratamiento`;
+  async remove(id: number):Promise<Tratamiento> {
+    const tratamiento = await this.findOne(id);
+    return this.tratamientosRepository.softRemove(tratamiento);
   }
 }
