4e9e3a4cebcbdc18500db7b6031ac24e885f5b94
Josue Pomacusi <pomacusi7@gmail.com>
2025-10-15 18:40:23 -0400

    feat: Dtos implementados con validacion de datos

--- backend_clinica_odontologica/src/tratamientos/dto/create-tratamiento.dto.ts
+++ backend_clinica_odontologica/src/tratamientos/dto/create-tratamiento.dto.ts
@@ -1,1 +1,16 @@
-export class CreateTratamientoDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsPositive, IsString, MaxLength } from "class-validator";
+
+export class CreateTratamientoDto {
+    @ApiProperty()
+    @IsNotEmpty({ message: 'La descripción es obligatoria' })
+    @IsString({ message: 'La descripción debe ser texto' })
+    @MaxLength(100, { message: 'La descripción no debe superar los 100 caracteres' })
+    descripcion: string;
+
+    @ApiProperty()
+    @IsNotEmpty({ message: 'El costo es obligatorio' })
+    @IsNumber({}, { message: 'El costo debe ser numérico' })
+    @IsPositive({ message: 'El costo debe ser un número positivo' })
+    costo: number;
+}
--- backend_clinica_odontologica/src/tratamientos/dto/update-tratamiento.dto.ts
+++ backend_clinica_odontologica/src/tratamientos/dto/update-tratamiento.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
+
+import { PartialType } from '@nestjs/swagger';
 import { CreateTratamientoDto } from './create-tratamiento.dto';
 
 export class UpdateTratamientoDto extends PartialType(CreateTratamientoDto) {}
